d14eb587dae8550a323db4a9ef27baf4a5572535
Adlen CHERIF <Adlen.Cherif@etu.univ-savoie.fr>
2022-06-19 16:06:23 +0200

    correction + tests (début)

--- Gestion_Animaux/Frames/Main/Main.xaml.cs
+++ Gestion_Animaux/Frames/Main/Main.xaml.cs
@@ -1,5 +1,4 @@
-﻿using CefSharp.Wpf;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
---
+++ Gestion_AnimauxTests/MainWindowTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gestion_Animaux;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gestion_Animaux.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        [TestMethod()]
+        public void MainWindowTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
